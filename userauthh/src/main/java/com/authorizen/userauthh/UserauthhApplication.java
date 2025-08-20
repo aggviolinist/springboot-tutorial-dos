32ef036b716c35aa63be7a886cf07c38b7bcf8c8
sCuLpToR 699 <mulandimaei76@gmail.com>
2025-08-20 08:53:48 +0000

    jwt project

---
+++ userauthh/src/main/java/com/authorizen/userauthh/UserauthhApplication.java
@@ -0,0 +1,13 @@
+package com.authorizen.userauthh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserauthhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserauthhApplication.class, args);
+	}
+
+}
