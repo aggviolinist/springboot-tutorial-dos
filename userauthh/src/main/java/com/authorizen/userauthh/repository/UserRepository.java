d1a9b0700310dda1a4d7e32e5e3a40bd695636a4
sCuLpToR 699 <mulandimaei76@gmail.com>
2025-08-20 12:31:11 +0000

    jwt auth

---
+++ userauthh/src/main/java/com/authorizen/userauthh/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.authorizen.userauthh.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.authorizen.userauthh.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
