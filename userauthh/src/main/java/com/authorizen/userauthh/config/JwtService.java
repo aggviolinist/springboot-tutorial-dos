ab4df3659eee699b5e53eabcfaccf84b86482ec1
sCuLpToR <72200097+aggviolinist@users.noreply.github.com>
2025-08-26 14:24:16 +0000

    jwt image

--- userauthh/src/main/java/com/authorizen/userauthh/config/JwtService.java
+++ userauthh/src/main/java/com/authorizen/userauthh/config/JwtService.java
@@ -33,7 +33,8 @@ public class JwtService {
             .claims(extraClaims)
             .subject(userDetails.getUsername())
             .issuedAt(new Date(System.currentTimeMillis()))
-            .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24)) // Set expiration
+            .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+            //.expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24)) // Set expiration
             .signWith(getSignInKey())
             .compact();
     }
