49838981191499ae349e2a1b577a95161b3aa7ee
sCuLpToR 699 <mulandimaei76@gmail.com>
2025-08-28 09:46:40 +0000

    password matches

---
+++ userauthh/src/main/java/com/authorizen/userauthh/auth/PasswordMatchesValidator.java
@@ -0,0 +1,16 @@
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import com.authorizen.userauthh.dto.RegisterRequest; // Your DTO
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+
+    @Override
+    public boolean isValid(Object obj, ConstraintValidatorContext context) {
+        if (obj instanceof RegisterRequest) {
+            RegisterRequest user = (RegisterRequest) obj;
+            // Compare password with confirmPassword
+            return user.getPassword() != null && user.getPassword().equals(user.getConfirmPassword());
+        }
+        return false;
+    }
+}
