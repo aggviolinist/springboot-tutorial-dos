107ce2dea6915ab17588492f8489dcf6bc37ee73
sCuLpToR <72200097+aggviolinist@users.noreply.github.com>
2025-09-19 19:24:34 +0000

    adminkontrola

--- userauthh/src/main/java/com/authorizen/userauthh/admin/ManagementKontrola.java
+++ userauthh/src/main/java/com/authorizen/userauthh/admin/ManagementKontrola.java
@@ -2,30 +2,29 @@ package com.authorizen.userauthh.admin;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/admin")
-public class AdministratorKontrolla {
+@RequestMapping("/api/v1/management")
+public class ManagementKontrola {
     @GetMapping
     public String get(){
-        return "GET:: Admin Controller";
+        return "GET:: Management controller";
     }
     @PostMapping
     public String post(){
-        return "POST:: Admin Controller";
+        return "POST:: Management controller";
     }
     @PutMapping
     public String put(){
-        return "PUT:: Admin Controller";
+        return "PUT:: Management controller";
     }
     @DeleteMapping
     public String delete(){
-        return "DELETE:: Admin Controller";
+        return "DELETE:: Management controller";
     }
-
     
 }
--- userauthh/src/main/java/com/authorizen/userauthh/model/Role.java
+++ userauthh/src/main/java/com/authorizen/userauthh/model/Role.java
@@ -2,5 +2,6 @@ package com.authorizen.userauthh.model;
 
 public enum Role {
     USER,
-    ADMIN
+    ADMIN,
+    MANAGER
 }
