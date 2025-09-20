58f68ab53b9a10072ea48cfba617d5dca9079d84
sCuLpToR <72200097+aggviolinist@users.noreply.github.com>
2025-09-20 09:12:36 +0000

    roless

--- userauthh/src/main/java/com/authorizen/userauthh/model/Role.java
+++ userauthh/src/main/java/com/authorizen/userauthh/model/Role.java
@@ -1,7 +1,33 @@
 package com.authorizen.userauthh.model;
 
+import java.util.Collections;
+import java.util.Set;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum Role {
-    USER,
-    ADMIN,
-    MANAGER
+    USER(Collections.emptySet()),
+    ADMIN(
+        Set.of(
+            permissions.ADMIN_READ,
+            permissions.ADMIN_CREATE,
+            permissions.ADMIN_UPDATE,
+            permissions.ADMIN_DELETE
+
+        )       
+    ),
+    MANAGER(
+        Set.of(
+            permissions.MANAGER_READ,
+            permissions.MANAGER_CREATE,
+            permissions.MANAGER_UPDATE,
+            permissions.MANAGER_DELETE
+        )
+    );
+
+    @Getter
+    private final Set<Permissions> permissions;
+
 }
